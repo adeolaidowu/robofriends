15dcbd162cb11d28d16a2aca936f745dc709f0d3
adeola <adeolaidowu27@gmail.com>
2020-02-21 00:13:41 +0100

    Updates

---
+++ precache-manifest.1872e315f7b30e5ed8522ba55a1e25b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b084a84d7ff021e55854cd8424c48b8",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "165c83f85a4584eb65d7",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "8718c7c9ff625a03c6a5",
+    "url": "/robofriends/static/css/main.09f5a115.chunk.css"
+  },
+  {
+    "revision": "165c83f85a4584eb65d7",
+    "url": "/robofriends/static/js/2.590933e2.chunk.js"
+  },
+  {
+    "revision": "8718c7c9ff625a03c6a5",
+    "url": "/robofriends/static/js/main.46013736.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
